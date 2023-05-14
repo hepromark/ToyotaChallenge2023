0468f831475902e09ebb4e6dae904ae2eb02ef4b
hepromark <markdo28102004@gmail.com>
2023-05-14 12:05:31 -0400

    sussy

--- Cv_to_pytorch.py
+++ Cv_to_pytorch.py
@@ -3,12 +3,11 @@ import cv2
 import numpy as np
 import torch
 
-cap = cv2.VideoCapture(1)
+cap = cv2.VideoCapture(0)
 
 while True:
     ret, frame = cap.read()
     frame = cv2.resize(frame, dsize=(640, 640))
-    print(frame)
     CHW = np.transpose(frame, (2, 0, 1))
     # blob = cv2.dnn.blobFromImage(frame, 1, size=size,swapRB=True)
     cv2.imshow('frame', frame)
